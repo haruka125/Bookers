22e5b1954df12ca037bb3ff9ecd2f111b05f062f
haruka125 <122009418+haruka125@users.noreply.github.com>
2023-02-01 14:34:46 +0000

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'lists/new'
   get 'lists/show'
   get 'lists/edit'
-  get 'index' => 'books#index'
+  get 'books' => 'books#index'
   get 'top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
